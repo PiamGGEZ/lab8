f606da17ebb6cbd0ed70b3b82e171a6b14522003
Piam <piumchoke@gmail.com>
2024-12-06 17:23:40 +0700

    completed lab8_3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,49 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+using namespace std ;
+int age,tall,bouty;
+
+int main(){
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 25){
+        cout << "Enter your height: ";
+        cin >> tall;
+        if (tall < 100){
+            cout << "Your character = Chopper" ; 
+        }
+        else if (tall < 180){
+            cout << "Your character = Usopp" ; 
+        }
+        else if (tall >= 180){
+            cout << "Enter your bounty: ";
+            cin >> bouty;
+            if (bouty > 1100000000){
+                cout << "Your character = Zoro" ; 
+            }
+            else if (bouty <= 1100000000){
+                cout << "Your character = Sanji" ; 
+            }
+           
+        }
+
+    }
+else if (age > 25 && age <=60 ){
+            cout << "Enter your bounty: ";
+            cin >> bouty;
+            if (bouty > 500000000){
+                cout << "Your character = Jinbe" ; 
+            }
+            else if (bouty <= 500000000){
+                cout << "Your character = Franky" ; 
+            }
+
+    }
+else if (age > 60) {
+        cout << "Your character = Brook" ; 
+    }
+    
+}
+
+
+
